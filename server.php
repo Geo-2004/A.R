227391ed2c13c52b410d2ba5078e3a84587a8f1b
Geovany <rikimamallacta5@gmail.com>
2025-02-01 20:45:36 -0500

    Fix: configuración del servidor

--- server.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = '0.0.0.0'; 
-$port = getenv('PORT') ?: 8080; // Usa el puerto de Railway o 3000 por defecto
-
-echo "Servidor corriendo en http://$host:$port\n";
-exec("php -S $host:$port -t public"); // Cambia 'public' si tu index.php está en otra carpeta
-?>

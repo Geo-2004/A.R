4bf2eb5b7f0a4e07d1970869ca25b3bcf72418ac
Geo-2004 <rikimamallacta5@gmail.com>
2025-02-06 03:45:57 -0500

    actualizar

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__.'../router.php'; // Redirige al sistema
+require_once(__DIR__ . '/../router.php');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
